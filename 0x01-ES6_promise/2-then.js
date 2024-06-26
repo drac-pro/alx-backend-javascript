1bde2b416d3e03cc4c66a274c2208031abab8da8
Darius Tohtin <dariustohtin@gmail.com>
2024-06-26 18:12:38 +0000

    task 2. Catch me if you can!

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,5 +2,5 @@ export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({ status: 200, body: 'Success' }))
     .then(() => { console.log('Got a response from the API'); })
-    .catch(() => Error());
+    .catch(() => {return Error();});
 }
