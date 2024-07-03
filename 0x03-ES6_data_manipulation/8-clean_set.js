1eef0204daa1666c03b41021974d794fadd99a93
Darius Tohtin <dariustohtin@gmail.com>
2024-07-03 03:17:15 +0000

    Edited 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   if (startString.length === 0) return '';
   return Array.from(set)
-    .filter((str) => str.startsWith(startString))
-    .map((str) => str.slice(startString.length))
+    .filter((str) => (str !== undefined ? str.startsWith(startString) : ''))
+    .map((str) => (str !== undefined ? str.slice(startString.length) : ''))
     .join('-');
 }
