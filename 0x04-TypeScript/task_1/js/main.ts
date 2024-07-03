08251877363d5a3b9c25628e09cf7b05172d8c05
Darius Tohtin <dariustohtin@gmail.com>
2024-07-03 23:59:18 +0000

    task 1. Let's build a Teacher interface .. 4. Writing a class

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,48 @@
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any;
+}
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) =>
+    `${firstName.charAt(0)}. ${lastName}`;
+
+interface StudentConstructor {
+    new (firstName: string, lastName: string): Student;
+}
+
+interface Student {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements Student {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
