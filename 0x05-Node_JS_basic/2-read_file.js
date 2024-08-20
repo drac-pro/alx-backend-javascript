d81cbb745ffa882cc194566970ffc209119ffc56
Darius Tohtin <dariustohtin@gmail.com>
2024-08-20 22:57:54 +0100

    task 2. Reading a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,43 @@
+const fs = require('node:fs');
+
+/**
+ * attempt to read the database file synchronously
+ * @param {path} path - path to the database file
+ */
+function countStudents(path) {
+  try {
+    const fileContent = fs.readFileSync(path, 'utf-8');
+    const lines = fileContent.trim().split('\n');
+    if (lines.length === 0) {
+      throw new Error('Cannot load the database');
+    }
+
+    const headers = lines[0].split(',');
+    const fieldIndex = headers.indexOf('field');
+    const firstNameIndex = headers.indexOf('firstname');
+
+    const fieldCount = {};
+    let totalStudents = 0;
+
+    for (let i = 1; i < lines.length; i += 1) {
+      const line = lines[i];
+      const columns = line.split(',');
+      const field = columns[fieldIndex];
+      const firstName = columns[firstNameIndex];
+
+      if (!fieldCount[field]) {
+        fieldCount[field] = [];
+      }
+      fieldCount[field].push(firstName);
+      totalStudents += 1;
+    }
+    console.log(`Number of students: ${totalStudents}`);
+    for (const [field, names] of Object.entries(fieldCount)) {
+      console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
