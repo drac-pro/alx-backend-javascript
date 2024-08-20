4e1f54ac7d7be046daf75d1fa3c5da7ce0baf48a
Darius Tohtin <dariustohtin@gmail.com>
2024-08-20 17:01:01 +0100

    used process.stdout.write

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,8 +4,7 @@
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
+  process.stdout.write(`Your name is: ${data}`);
   process.exit();
 });
 
