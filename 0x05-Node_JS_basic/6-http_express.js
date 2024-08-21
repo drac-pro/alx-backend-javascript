6742aaa7909595132343cc8f26d80054e3b6bff1
Darius Tohtin <dariustohtin@gmail.com>
2024-08-21 16:49:53 +0100

    task 6. Create a small HTTP server using Express

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,15 @@
+/**
+ * Creates a simple app using express.
+ */
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (_, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+  console.log(`Simple app listening on port ${port}`);
+});
