18851e67ba8a1d04e2c106d12f844ec1cb6a0157
Darius Tohtin <dariustohtin@gmail.com>
2024-08-22 20:39:41 +0100

    task 9. Regex integration testing

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,61 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const { expect } = chai;
+
+chai.use(chaiHttp);
+
+describe('index Page', () => {
+  const serverUrl = 'http://localhost:7865';
+
+  it('check correct status code', () => new Promise((done) => {
+    chai.request(serverUrl)
+      .get('/')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        done();
+      });
+  }));
+
+  it('check correct result', () => new Promise((done) => {
+    chai.request(serverUrl)
+      .get('/')
+      .end((err, res) => {
+        expect(res.text).to.equal('Welcome to the payment system');
+        done();
+      });
+  }));
+
+  it('check correct Content-Type', () => new Promise((done) => {
+    chai.request(serverUrl)
+      .get('/')
+      .end((err, res) => {
+        expect(res).to.have.header('content-type', /html/);
+        done();
+      });
+  }));
+});
+
+describe('cart Page', () => {
+  const serverUrl = 'http://localhost:7865';
+
+  it('check correct status code when :id is a number', () => new Promise((done) => {
+    chai.request(serverUrl)
+      .get('/cart/123')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.text).to.equal('Payment methods for cart 123');
+        done();
+      });
+  }));
+
+  it('check 404 status code when :id is NOT a number', () => new Promise((done) => {
+    chai.request(serverUrl)
+      .get('/cart/abc')
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+        expect(res.text).to.equal('Invalid cart ID');
+        done();
+      });
+  }));
+});
