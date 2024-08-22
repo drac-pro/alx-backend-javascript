b347b17b67e7f8f337e5e3ff8ef3bda5dc4aad1d
Darius Tohtin <dariustohtin@gmail.com>
2024-08-22 15:22:06 +0100

    1. Combining descriptions

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,24 @@
+/**
+ * round two numbers and return the result of the operation
+ * @param {string} type - operation to be performed
+ * @param {number} a - first number
+ * @param {number} b - second number
+ */
+function calculateNumber(type, a, b) {
+  const roundedA = Math.round(a);
+  const roundedB = Math.round(b);
+
+  switch (type) {
+    case 'SUM':
+      return roundedA + roundedB;
+    case 'SUBTRACT':
+      return roundedA - roundedB;
+    case 'DIV':
+      if (roundedB === 0) return 'Error';
+      return roundedA / roundedB;
+    default:
+      throw new Error('Invalid type');
+  }
+}
+
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,32 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+  describe('sUM', () => {
+    it('check sum of rounded numbers', () => {
+      assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+      assert.strictEqual(calculateNumber('SUM', 1.2, 2.3), 3);
+      assert.strictEqual(calculateNumber('SUM', 1.5, 2.5), 5);
+    });
+  });
+
+  describe('sUBTRACT', () => {
+    it('check subtraction of rounded numbers', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+      assert.strictEqual(calculateNumber('SUBTRACT', 3.2, 2.3), 1);
+      assert.strictEqual(calculateNumber('SUBTRACT', 5.5, 2.5), 3);
+    });
+  });
+
+  describe('dIVIDE', () => {
+    it('check division of rounded numbers', () => {
+      assert.strictEqual(calculateNumber('DIV', 7.4, 2.4), 3.5);
+      assert.strictEqual(calculateNumber('DIV', -5.6, 2.1), -3);
+    });
+
+    it('check "Error" when dividing by 0', () => {
+      assert.strictEqual(calculateNumber('DIV', 1.4, 0.2), 'Error');
+      assert.strictEqual(calculateNumber('DIV', 7.0, 0), 'Error');
+    });
+  });
+});
